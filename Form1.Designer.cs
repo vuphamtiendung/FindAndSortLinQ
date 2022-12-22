3eec6dcd7dde77b8feae8388576fd692fa5cf17e
Vũ Phạm Tiến Dũng <43530592+vuphamtiendung@users.noreply.github.com>
2022-12-23 02:12:36 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace FindAndSort04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lsvListProduct = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvListProduct = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lsvListProduct);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(517, 501);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lọc danh sách bình thường";
+            // 
+            // lsvListProduct
+            // 
+            this.lsvListProduct.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lsvListProduct.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsvListProduct.FullRowSelect = true;
+            this.lsvListProduct.GridLines = true;
+            this.lsvListProduct.HideSelection = false;
+            this.lsvListProduct.Location = new System.Drawing.Point(3, 18);
+            this.lsvListProduct.Name = "lsvListProduct";
+            this.lsvListProduct.Size = new System.Drawing.Size(511, 480);
+            this.lsvListProduct.TabIndex = 0;
+            this.lsvListProduct.UseCompatibleStateImageBehavior = false;
+            this.lsvListProduct.View = System.Windows.Forms.View.Details;
+            this.lsvListProduct.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.lsvListProduct_ColumnClick);
+            this.lsvListProduct.SelectedIndexChanged += new System.EventHandler(this.lsvListProduct_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã sản phẩm";
+            this.columnHeader1.Width = 123;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên sản phẩm";
+            this.columnHeader2.Width = 207;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Đơn giá";
+            this.columnHeader3.Width = 160;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvListProduct);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(535, 14);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(532, 501);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Lọc danh sách dùng Anonymous Type";
+            // 
+            // dgvListProduct
+            // 
+            this.dgvListProduct.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvListProduct.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvListProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListProduct.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvListProduct.Location = new System.Drawing.Point(3, 18);
+            this.dgvListProduct.Name = "dgvListProduct";
+            this.dgvListProduct.Size = new System.Drawing.Size(526, 480);
+            this.dgvListProduct.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 527);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Quản lý sản phẩm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView lsvListProduct;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvListProduct;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindAndSort04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        List<SanPham> listProduct = null;
+
+        public void DisplayProduct()
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            listProduct = (from x in context.SanPhams
+                           select x).ToList();
+            lsvListProduct.Items.Clear();
+            listProduct.ForEach(x =>
+            {
+                ListViewItem item = new ListViewItem(x.MaSP + "");
+                item.SubItems.Add(x.TenSP);
+                item.SubItems.Add(x.DonGia + "");
+                lsvListProduct.Items.Add(item);
+            });
+        }
+
+        public void DataProduct()
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext();
+            var dataProduct = (from i in context.SanPhams
+                               select new
+                               {
+                                   i.MaSP,
+                                   i.TenSP
+                               }).ToList();
+            dgvListProduct.DataSource = dataProduct;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayProduct();
+            DataProduct();
+        }
+
+        private void lsvListProduct_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            ColumnHeader column = lsvListProduct.Columns[e.Column];
+            if (e.Column == 0)
+            {
+                if (column.Tag == null || column.Tag.ToString() == "des")
+                {
+                    listProduct = listProduct.OrderBy(x => x.MaSP).ToList();
+                    column.Tag = "asc";
+                }
+                else
+                {
+                    listProduct = listProduct.OrderByDescending(x => x.MaSP).ToList();
+                    column.Tag = "des";
+                }
+            }
+            else if(e.Column == 1)
+            {
+                if(column.Tag == null || column.Tag.ToString() == "des")
+                {
+                    listProduct = listProduct.OrderBy(x => x.TenSP).ToList();
+                    column.Tag = "asc";
+                }
+                else
+                {
+                    listProduct = listProduct.OrderByDescending(x => x.TenSP).ToList();
+                    column.Tag = "des";
+                }
+            }
+            else if(e.Column == 2)
+            {
+                if(column.Tag == null || column.Tag.ToString() == "des")
+                {
+                    listProduct = listProduct.OrderBy(x => x.DonGia).ToList();
+                    column.Tag = "asc";
+                }
+                else
+                {
+                    listProduct = listProduct.OrderByDescending(x => x.DonGia).ToList();
+                    column.Tag = "des";
+                }
+            }
+            lsvListProduct.Items.Clear();
+            listProduct.ForEach(x =>
+            {
+                ListViewItem item = new ListViewItem(x.MaSP + "");
+                item.SubItems.Add(x.TenSP);
+                item.SubItems.Add(x.DonGia + "");
+                lsvListProduct.Items.Add(item);
+            });
+        }
+
+        private void lsvListProduct_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
